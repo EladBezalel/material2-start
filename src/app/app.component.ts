5a2ccc9c74c5b414c647ae43b7440efd1da0f6b9
Elad Bezalel <elad.bezalel@gmail.com>
2016-11-10 00:56:27 +0200

    step-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from './models/User.model';
+import {UsersService} from './services/users.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  providers: [ UsersService ]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  users: Observable<User[]>;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.loadAll();
+  }
 }
