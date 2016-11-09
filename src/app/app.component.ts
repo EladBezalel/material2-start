6bf69d954b5c3ff85d4a584e028749aaa1907007
Elad Bezalel <elad.bezalel@gmail.com>
2016-11-10 00:57:37 +0200

    step-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,20 @@ import {User} from './models/User.model';
 import {UsersService} from './services/users.service';
 import {Observable} from 'rxjs';
 
+import {MdIconRegistry} from '@angular/material';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
-  providers: [ UsersService ]
+  providers: [ UsersService, MdIconRegistry ]
 })
 export class AppComponent implements OnInit {
   users: Observable<User[]>;
 
-  constructor(private usersService: UsersService) {}
+  constructor(mdIconRegistry: MdIconRegistry, private usersService: UsersService) {
+    mdIconRegistry.addSvgIconSetInNamespace('avatars', './assets/avatars.svg');
+  }
 
   ngOnInit(): void {
     this.users = this.usersService.loadAll();
