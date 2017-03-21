a5d8a26b68fe8b958130f4f4cec7fd0579b323aa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-03-21 21:02:56 +0100

    Step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import {AppComponent} from './app.component';
 
 @NgModule({
@@ -12,7 +16,10 @@ import {AppComponent} from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
