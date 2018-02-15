0f85176b47089ea31f06db0d858e4189c9154863
Juliano <juliano.brasil@gmail.com>
2018-02-15 10:25:33 -0200

    overall upgrade and fix small screen layout

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -5,7 +5,7 @@ import {MatDialogRef} from '@angular/material';
   templateUrl: 'dialog.component.html'
 })
 export class DialogComponent {
-  avatars = new Array(16).fill(0).map((_, i) => `svg-${i+1}`);
+  avatars = new Array(16).fill(0).map((_, i) => `svg-${i + 1}`);
   selectedAvatar = this.avatars[0];
 
   constructor(public dialogRef: MatDialogRef<DialogComponent>) {}
